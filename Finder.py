75053e6297ebf753debde31c0b58e5947942e18e
Ven0mGh0st <55100867+Ven0mGh0st@users.noreply.github.com>
2019-09-10 20:51:30 +0700

    Update Finder.py

--- Finder.py
+++ Finder.py
@@ -38,7 +38,7 @@ def main():
 
         
         ---------------------------------------------------------------
-        |                     GHOST EXPLOITER TEAM                    |
+        |                      G45 CYBER TEAM                         |
         |-------------------------------------------------------------|
         | AUTHOR           : VenomGhost [  ImamKun ]                  |
         | FACEBOOK         : Imam Ilyasa                              |
